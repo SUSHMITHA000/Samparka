a7d47f8669e8fdd750ebd49da34e2a8b2cca1dca
SushmitaRN <naiksushmita2003@gmail.com>
2025-12-17 00:41:19 +0530

    Help and chat fianl

--- app/src/main/java/com/example/samparka/ApiConfig.java
+++ app/src/main/java/com/example/samparka/ApiConfig.java
@@ -1,5 +1,5 @@
 package com.example.samparka;
 
 public class ApiConfig {
-    public static final String BASE_URL = "http://192.168.1.21:3000/chat"; // Paste your ipv4 adress of laptop here
+    public static final String BASE_URL = "ip_adress_here/chat"; // Paste your ipv4 adress of laptop here
 }
